340cfd9cdccab7d71884f73bd615539b0a199dae
ShockwaveComputer <dovershockwave@gmail.com>
2015-01-26 18:22:20 -0500

    Reworked button naming and added new speed functionality
    -Renamed buttons to what they are rather than what command they are
    bound to
    -Added SpeedUp and SpeedDown commands, which increase and decrease speed
    incrementally
    -Rebound the HalfSpeed and QuarterSpeed buttons to accomodate
    incremantal functionality

---
+++ src/org/usfirst/frc/team4546/robot2/commands/SpeedUp.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team4546.robot2.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team4546.robot2.Robot;
+
+public class SpeedUp extends Command {
+
+	protected void initialize() {
+		
+		if (Robot.speed <= .9)	{
+			
+			Robot.speed = Robot.speed + .1;
+		}
+	}
+
+	protected void execute() {
+		
+	}
+
+	protected boolean isFinished() {
+		return false;
+	}
+
+	protected void end() {
+		
+	}
+
+	protected void interrupted() {
+		
+	}
+
+}
+
