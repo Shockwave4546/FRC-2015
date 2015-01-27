c08b01f1f9a8fb10b74a740d3b04fce14bfb0ba7
ShockwaveComputer <dovershockwave@gmail.com>
2015-01-27 08:23:58 -0500

    Added limit switches to stop the lift at the top and bottom, and also added several values to the dashboard

--- src/org/usfirst/frc/team4546/robot2/commands/LiftDown.java
+++ src/org/usfirst/frc/team4546/robot2/commands/LiftDown.java
@@ -40,19 +40,19 @@ public class  LiftDown extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Robot.lift.isFullyDown();
     }
 
     // Called once after isFinished returns true
     protected void end() {
     	
-    
+    	Robot.lift.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
     	
-    
+    	Robot.lift.stop();
     }
 }
