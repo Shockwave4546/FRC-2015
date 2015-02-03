0dc2e947af85dde210d79119dd5769f9e5d4612d
ShockwaveComputer <dovershockwave@gmail.com>
2015-02-03 14:49:50 -0500

    Changed angle input to 0

--- src/org/usfirst/frc/team4546/robot2/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team4546/robot2/subsystems/Drivetrain.java
@@ -45,8 +45,8 @@ public class Drivetrain extends Subsystem {
     }
 
 
-	public void mecanumDrive(double x, double y, double z, double angle, double speed) {
-		chassis.mecanumDrive_Cartesian(x*speed, y*speed, z*speed, angle);
+	public void mecanumDrive(double x, double y, double z, double speed) {
+		chassis.mecanumDrive_Cartesian(x*speed, y*speed, z*speed, 0);
 		
 	}
 	
